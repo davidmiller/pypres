d39ef001b6086c483bbfe9113b08b68c2bf9a953
David Miller <David>
2012-03-13 23:28:19 +0000

    Working presentation script for sample presentation

--- pypres/__init__.py
+++ pypres/__init__.py
@@ -14,9 +14,10 @@ def main():
         description = "Presenting"
         )
     parser.add_argument("presentation",  help = "Presentation file to run")
+    parser.add_argument("-s", "--slide", help = "Start at slide num (0 indexed)", default = 0)
     args =  parser.parse_args()
 
     sys.ps1 = '{0}.{1} >>> '.format( *sys.version_info)
-    replish = PresentConsole(args.presentation)
+    replish = PresentConsole(args.presentation, slidenum=int(args.slide))
     replish.interact()
 
--- pypres/pres.py
+++ pypres/pres.py
@@ -11,35 +11,90 @@ class PresentConsole(code.InteractiveConsole):
 
     Empty newlines execute the next block of python code...
     """
-    def __init__(self, prespath, *args, **kwargs):
+    def __init__(self, prespath, slidenum=0, *args, **kwargs):
         self._prespath = prespath
-        self._slidenum = 0
+        self._slidenum = slidenum
         code.InteractiveConsole.__init__(self, *args, **kwargs)
         with open(prespath, 'r') as presfile:
             self.slides = presfile.read().split("#--newslide")
+
+    def statusing(self):
+        """
+        Print the status of our presentation
+        """
+        status = "Slide {0} of {1}".format(self._slidenum, len(self.slides))
+        print(status)
+        return
+
+    def codeslide(self, slide):
+        """
+        Runs a slide with some code in it.
+
+        Arguments:
+        - `slide`: str
+        """
+        stripped = slide[7:].strip()
+        for x in stripped.split("\n"):
+            print(sys.ps1 + x)
+            super(PresentConsole, self).push(x)
+        return
+
+    def simpleslide(self, slide):
+        """
+        This is text rite? I guess we'll just print() it
+
+        Arguments:
+        - `slide`: str
+        """
+        print(slide)
+        return
+
+    def splitslide(self, slide):
+        """
+        This slide wants to get printed bit by bit.
+
+        Let's do that.
+
+        Arguments:
+        - `slide`:
+        """
+        slide = slide.strip()
+        sections = slide[8:].split('#----')
+        for i, section in enumerate(sections):
+            if section == '':
+                continue
+            print(section.strip())
+            if i + 1 < len(sections):
+                input = self.raw_input()
+                if input != '':
+                    raise ValueError("Entered code in a splitside. Stop it.")
+        return
+
+    def prevslide(self):
+        """
+        Go back one slide!
+        """
+        self._slidenum -= 2
+        return self.runslide("")
 
     def runslide(self, line):
         """
         Print then run a slide
         """
-        try:
-            os.system('clear')
-            #import pdb; pdb.set_trace()
-            if line.strip() == "^":
-                slide = self.slides[self._slidenum - 1].strip()
-                self._slidenum -= 1
-            else:
-                slide =  self.slides[self._slidenum].strip()
-                self._slidenum += 1
-            if slide.startswith("#--code"):
-                print(sys.ps1 + slide[7:].strip())
-                return super(PresentConsole, self).push(slide)
-            print(slide)
-        except IndexError: # No more slides
-            if self._slidenum != len(self.slides):
-                raise
-        return
-
+        if self._slidenum >= len(self.slides):
+            return
+        os.system('clear')
+        if line.strip() == "^":
+            return self.prevslide()
+        else:
+            slide =  self.slides[self._slidenum].strip()
+            self._slidenum += 1
+        self.statusing()
+        if slide.startswith("#--code"):
+            return self.codeslide(slide)
+        if slide.startswith('#--split'):
+            return self.splitslide(slide)
+        return self.simpleslide(slide)
 
     def push(self, line):
         """
